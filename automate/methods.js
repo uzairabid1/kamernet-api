1ddf98cd5d63723c844a46406babf1a16f42702b
unknown <uzairabid222@gmail.com>
2024-03-11 19:59:00 +0500

    cookies fix

--- automate/methods.js
+++ automate/methods.js
@@ -10,10 +10,13 @@ async function login(page, username, password) {
 
     try {
         await delay(2000);
-        await page.waitForXPath("//button[.='Accept all']");
-        const [cookieButton] = await page.$x("//button[.='Accept all']");
-        await cookieButton.evaluate(cookieButton => cookieButton.click());
-        console.log('Cookie accepted')
+        try{
+            const [cookieButton] = await page.$x("//button[.='Accept all']");
+            await cookieButton.evaluate(cookieButton => cookieButton.click());
+            console.log('Cookie accepted');
+        }catch(err){
+            console.log('No cookies found')
+        }
         //click on login button
         await page.waitForXPath("//a[.='Log in']");
         const [loginButton] = await page.$x("//a[.='Log in']");
